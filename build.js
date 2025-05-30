235d38d32d7e302126ed48f39de189841cc6fa5a
Luke Leber <lal65@psu.edu>
2025-05-30 16:37:22 -0400

    Let's try this..?

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
 fs.mkdirSync('./dist', {recursive: true});
+fs.mkdirSync('./docs', {recursive: true});
 const css = fs.readFileSync('./src/a11y-select.css', 'utf8');
 const CleanCSS = require('clean-css');
 fs.writeFileSync('./dist/a11y-select.min.css', new CleanCSS().minify(css).styles);
+fs.writeFileSync('./docs/a11y-select.min.css', new CleanCSS().minify(css).styles);
 
 const js = fs.readFileSync('./src/a11y-select.mjs', 'utf8');
 const UglifyJS = require('uglify-js');
 fs.writeFileSync('./dist/a11y-select.min.mjs', UglifyJS.minify(js).code);
-fs.writeFileSync('./dist/a11y-select.min.js', UglifyJS.minify(js).code);
+fs.writeFileSync('./docs/a11y-select.min.mjs', UglifyJS.minify(js).code);
